f9574c20de30b2ddf04d2018da5e7618100c8f30
khalid <khalidfuad96@gmail.com>
2022-11-03 20:07:21 +0000

    update

---
+++ src/components/HelloDiv.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function HelloDiv() {
+  return (
+    <div>
+      <h1>Hey guys! my name is top g</h1>
+      <h2>Things I like!</h2>
+      <ul>
+        <li>lamborghini</li>
+        <li>blue one </li>
+        <li>red one</li>
+      </ul>
+    </div>
+  );
+}
+
+export default HelloDiv;
