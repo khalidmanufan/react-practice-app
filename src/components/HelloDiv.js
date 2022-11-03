e41fb8c6aa6e6b21f27b04619bc96133454fb34f
khalid <khalidfuad96@gmail.com>
2022-11-03 20:12:35 +0000

    new

--- src/components/HelloDiv.js
+++ src/components/HelloDiv.js
@@ -3,12 +3,12 @@ import React from "react";
 function HelloDiv() {
   return (
     <div>
-      <h1>Hey guys! my name is top g</h1>
+      <h1>Hey guys! my name is aka khal</h1>
       <h2>Things I like!</h2>
       <ul>
         <li>lamborghini</li>
         <li>blue one </li>
-        <li>red one</li>
+        <li> and red one</li>
       </ul>
     </div>
   );
