5e118f3f1bc430d4385f7a7460a19c42bf325ca6
khalid <khalidfuad96@gmail.com>
2022-11-03 21:09:57 +0000

    props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import HelloDiv from "./components/HelloDiv";
+import JSXVariables from "./components/JSXVariables";
+import Card from "./components/Card";
 
 function App() {
-  return <HelloDiv />;
+  return <Card />;
 }
 
 export default App;
